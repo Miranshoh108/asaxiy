09b94f66579d818e3f0a6cca18d008eae82d2cf2
Muhammad <miranshoh108@gamil.com>
2025-01-21 23:54:33 +0500

    done

--- js/admin.js
+++ js/admin.js
@@ -1,79 +1,69 @@
-
-
 if (!localStorage.getItem("user")) {
   window.location.href = "../src/login.html";
 }
 
 import { useFetch } from "./utils/request.js";
 import { addUIData } from "./utils/ui.js";
+
 const post_product = document.getElementById("post_product");
-let getId = document.getElementById("getId");
-let btns = document.querySelectorAll(".btn");
-let addForm = document.querySelector(".add");
-let deleteForm = document.querySelector(".delete");
-let editForm = document.querySelector(".edit");
-let carts = document.querySelector(".carts");
-let deleteBtn = document.querySelector(".delete-btn");
-let id = localStorage.getItem("id") || "add";
+const getId = document.getElementById("getId");
+const getIdEdit = document.getElementById("getIdEdit");
+const post_product_edit = document.getElementById("post_product_edit");
+
+const btns = document.querySelectorAll(".btn");
+const addForm = document.querySelector(".add");
+const deleteForm = document.querySelector(".delete");
+const editForm = document.querySelector(".edit");
+
+const carts = document.querySelector(".carts");
+const carts2 = document.querySelector(".carts2");
+
+const deleteBtn = document.querySelector(".delete-btn");
+const signOut = document.querySelector(".sign-out");
+const name = document.querySelector(".name");
+
 let users = JSON.parse(localStorage.getItem("user"));
 const request = useFetch();
 
-// header
-
-let name = document.querySelector(".name");
-let signOut = document.querySelector(".sign-out");
 name.innerHTML = users.username;
 signOut.addEventListener("click", () => {
   localStorage.removeItem("user");
   window.location.href = "../src/index.html";
 });
 
-// header
+// Formalarni boshida yashirish
+addForm.style.display = "none";
+deleteForm.style.display = "none";
+editForm.style.display = "none";
 
-btns.forEach((value) => {
-  value.addEventListener("click", (e) => {
-    localStorage.setItem("id", e.target.id);
-    editUi(e.target.id);
+// Tugmalarni bosganda tegishli bo‘limni ochish
+btns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    let id = e.target.id;
+    localStorage.setItem("id", id);
+    showSection(id);
   });
 });
 
-function editUi(id) {
-  switch (id) {
-    case "add":
-      addForm.style.display = "block";
-      deleteForm.style.display = "none";
-      editForm.style.display = "none";
-      break;
-    case "delete":
-      addForm.style.display = "none";
-      deleteForm.style.display = "block";
-      editForm.style.display = "none";
-      break;
-
-    default:
-      addForm.style.display = "none";
-      deleteForm.style.display = "none";
-      editForm.style.display = "block";
-      break;
-  }
+function showSection(id) {
+  addForm.style.display = id === "add" ? "block" : "none";
+  deleteForm.style.display = id === "delete" ? "block" : "none";
+  editForm.style.display = id === "edit" ? "block" : "none";
 }
+
+// Mahsulot qo‘shish
 post_product.addEventListener("submit", (e) => {
   e.preventDefault();
-  console.log(post_product.img_file.files[0].type);
+  let file = post_product.img_file.files[0];
 
-  let imgUrl = "";
-  if (
-    post_product.img_file.files[0].type !== "image/png" &&
-    post_product.img_file.files[0].type !== "image/webp"
-  ) {
-    return alert("Iltimos png krting");
+  if (file.type !== "image/png" && file.type !== "image/webp") {
+    return alert("Iltimos faqat PNG yoki WEBP rasm yuklang");
   }
-  const fileReader = new FileReader();
-  fileReader.onload = function (event) {
-    imgUrl = event.target.result;
 
+  const reader = new FileReader();
+  reader.onload = function (event) {
     let newData = {
-      img: imgUrl,
+      img: event.target.result,
       title: post_product.title.value,
       price: +post_product.price.value,
       month: +post_product.month.value,
@@ -81,103 +71,103 @@ post_product.addEventListener("submit", (e) => {
     };
     postData(newData);
   };
-  fileReader.readAsDataURL(post_product.img_file.files[0]);
+  reader.readAsDataURL(file);
 });
 
 function postData(data) {
-  request({ url: "asaxiy", method: "POST", data }).then(() =>
-    alert("Mahsulot qoshildi")
-  );
+  request({ url: "asaxiy", method: "POST", data }).then(() => {
+    alert("Mahsulot qo'shildi!");
+    post_product.reset();
+  });
 }
-editUi(id);
 
-// Delete start
-
+// Mahsulot o‘chirish
 deleteBtn.style.display = "none";
 getId.addEventListener("submit", (e) => {
   e.preventDefault();
   let id = getId.id.value;
   getOneProduct(id);
-  carts.innerHTML = "";
 });
 
 function getOneProduct(id) {
-  try {
-    request({ url: `asaxiy/${id}` }).then((data) => {
-      deleteBtn.style.display = "block";
-      deleteProductItem(data);
-    });
-  } catch (error) {}
-}
-
-function deleteProductItem(data) {
-  addUIData(data, carts);
-  deleteBtn.addEventListener("click", () => {
-    deleteProduct(data.id);
-    data = [];
+  request({ url: `asaxiy/${id}` }).then((data) => {
+    deleteBtn.style.display = "block";
+    addUIData(data, carts);
+    deleteBtn.onclick = () => deleteProduct(data.id);
   });
 }
+
 function deleteProduct(id) {
-  let sucsses = confirm("Ochirishingizga ishonchingiz komilmi ?");
-  if (sucsses) {
+  if (confirm("O‘chirishni tasdiqlaysizmi?")) {
     request({ url: `asaxiy/${id}`, method: "DELETE" }).then(() => {
-      alert("Malumot ochirildi !");
+      alert("Mahsulot o‘chirildi!");
       carts.innerHTML = "";
       deleteBtn.style.display = "none";
     });
-  } else {
-    alert("Malumot saqlanib qoldi !");
-    carts.innerHTML = "";
-    deleteBtn.style.display = "none";
   }
 }
 
-// Edit start
-let post_product_edit = document.getElementById("post_product_edit");
-let carts2 = document.querySelector(".carts2");
-let getIdEdit = document.getElementById("getIdEdit");
-
+// Mahsulotni o‘zgartirish
 getIdEdit.addEventListener("submit", (e) => {
   e.preventDefault();
   let id = getIdEdit.id.value;
-  getOneProduct2(id);
-  carts.innerHTML = "";
+  getOneProductForEdit(id);
 });
-function getOneProduct2(id) {
-  try {
-    request({ url: `asaxiy/${id}` }).then((data) => {
-      addUIData(data, carts2);
-      post_product_edit.style.display = "flex";
-      post_product_edit.addEventListener("submit", (e) => {
-        e.preventDefault();
-        let imgUrl = "";
-        if (
-          post_product_edit.img_file_edit.files[0].type !== "image/png" &&
-          post_product_edit.img_file_edit.files[0].type !== "image/webp"
-        ) {
-          return alert("Iltimos png krting");
-        }
-        const fileReader = new FileReader();
-        fileReader.onload = function (event) {
-          imgUrl = event.target.result;
 
-          let newData = {
-            img: imgUrl,
-            title: post_product_edit.title_edit.value,
-            price: +post_product_edit.price_edit.value,
-            month: +post_product_edit.month_edit.value,
-            rate: +post_product_edit.rate_edit.value,
-          };
-          editProduct(data.id, newData);
-        };
-        fileReader.readAsDataURL(post_product_edit.img_file_edit.files[0]);
+function getOneProductForEdit(id) {
+  request({ url: `asaxiy/${id}` }).then((data) => {
+    addUIData(data, carts2);
+    post_product_edit.style.display = "block";
+    editProductForm(data);
+  });
+}
+
+function editProductForm(data) {
+  post_product_edit.title_edit.value = data.title;
+  post_product_edit.price_edit.value = data.price;
+  post_product_edit.month_edit.value = data.month;
+  post_product_edit.rate_edit.value = data.rate;
+
+  post_product_edit.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let file = post_product_edit.img_file_edit.files[0];
+
+    if (file && file.type !== "image/png" && file.type !== "image/webp") {
+      return alert("Iltimos faqat PNG yoki WEBP rasm yuklang");
+    }
+
+    const reader = new FileReader();
+    reader.onload = function (event) {
+      let updatedData = {
+        img: event.target.result || data.img,
+        title: post_product_edit.title_edit.value,
+        price: +post_product_edit.price_edit.value,
+        month: +post_product_edit.month_edit.value,
+        rate: +post_product_edit.rate_edit.value,
+      };
+      updateProduct(data.id, updatedData);
+    };
+    if (file) {
+      reader.readAsDataURL(file);
+    } else {
+      updateProduct(data.id, {
+        img: data.img,
+        title: post_product_edit.title_edit.value,
+        price: +post_product_edit.price_edit.value,
+        month: +post_product_edit.month_edit.value,
+        rate: +post_product_edit.rate_edit.value,
       });
-    });
-  } catch (error) {}
+    }
+  });
 }
 
-function editProduct(id, newData) {
-  request({ url: `asaxiy/${id}`, method: "PUT", data: newData }).then(() =>
-    alert("Mahsulot ozgartrildi")
-  );
+function updateProduct(id, newData) {
+  request({ url: `asaxiy/${id}`, method: "PUT", data: newData }).then(() => {
+    alert("Mahsulot o‘zgartirildi!");
+    carts2.innerHTML = "";
+    post_product_edit.style.display = "none";
+  });
 }
+
+// Boshlang‘ich sahifani yangilash
+showSection(localStorage.getItem("id") || "add");
